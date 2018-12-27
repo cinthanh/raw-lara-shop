ebb02d7a2bac1a4cb7f0560454783f5f1ba04ae0
JsCore <thanhfgit@gmail.com>
2018-12-27 10:57:23 -0500

    required tree for project

---
+++ core/App.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: king
+ * Date: 12/27/18
+ * Time: 10:47 AM
+ */
+
+class App
+{
+
+}
---
+++ core/fn_common.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: king
+ * Date: 12/27/18
+ * Time: 10:45 AM
+ */
